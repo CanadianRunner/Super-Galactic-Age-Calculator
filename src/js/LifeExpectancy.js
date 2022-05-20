0c519ff3681b3ea05db48a7b21a81cc1d1ed3a64
Sean Keane <seankeane93@gmail.com>
2022-05-20 15:10:40 -0700

    add test to calculate when a user inputs female into the calculator

--- src/js/LifeExpectancy.js
+++ src/js/LifeExpectancy.js
@@ -3,6 +3,10 @@ import { LifeExpectancyAverage } from " ";
 describe(LifeExpectancyAverage, (test) => {
 
 test('It should change the life expectancy if the user inputs a gender of female', () => {
-  
-})
+  const lifeExpectancy = new LifeExpectancyAverage(true, true, "female");
+  lifeExpectancy.calculateLifeExpectancy();
+  expect(lifeExpectancy.lifeExpectancyAverage).toEqual(78);
+});
+
+
 });
