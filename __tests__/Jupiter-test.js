033b0710f8bc023f21403e051978a9ee64f3c141
Sean Keane <seankeane93@gmail.com>
2022-05-20 12:50:15 -0700

    add Jupiter life expectancy calc test

--- __tests__/Jupiter-test.js
+++ __tests__/Jupiter-test.js
@@ -2,6 +2,8 @@ import { JupiterLifeExpectancy} from "../src/js/Jupiter";
 
 describe('jupiterLifeExpectancy', () => {
   test('It should correctly create a Jupiter Life expectancy calculation based on inputted results', () => {
-
-  })
-})
+    const newLifeExpectancy = new JupiterLifeExpectancy('male', true, false);
+    newLifeExpectancy.lifeExpectancyGenderCalc();
+    expect(newLifeExpectancy.averageLifeExpectancy).toEqual();
+  });
+});
