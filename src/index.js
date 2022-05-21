e4f345b408500762744bc0a0491ac362486cd367
Sean Keane <seankeane93@gmail.com>
2022-05-21 15:35:59 -0700

    WIP trying to get code to bundle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-// entry point for project. 
-// user interface logic.
-// Import JS/CSS files here.
-// ie: import { PeanutButter } from './peanut-butter.js'
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { JupiterLifeExpectancy } from './js/Jupiter';
+import { LifeExpectancyAverage } from './js/LifeExpectancy';
+import { MarsLifeExpectancy} from './js/Mars';
+import { MercuryLifeExpectancy } from './js/Mercury';
+import { VenusLifeExpectancy } from './js/Venus';
